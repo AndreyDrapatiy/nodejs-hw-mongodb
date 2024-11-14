4113973d1bf9886e6d5f0855288a262d4ede8852
Andrey Drapatiy <drapatiy92@gmail.com>
2024-11-14 19:00:46 +0200

    Basic server setup

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
