0f73374412392707c2baaea53bd659e294697c2a
Andrey Drapatiy <drapatiy92@gmail.com>
2024-11-27 10:23:39 +0200

    Small Fixes

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,19 +1,19 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(1).max(20).required().messages({
+  name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Name should be a string',
     'string.min': 'Name should have at least {#limit} characters',
     'string.max': 'Name should have at most {#limit} characters',
     'any.required': 'Name is required',
   }),
-  phoneNumber: Joi.string().min(1).max(20).required().messages({
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Phone number should be a string',
     'string.min': 'Phone number should have at least {#limit} characters',
     'string.max': 'Phone number should have at most {#limit} characters',
     'any.required': 'Phone number is required',
   }),
-  email: Joi.string().min(1).max(20).email().messages({
+  email: Joi.string().min(3).max(20).email().messages({
     'string.base': 'Email should be a string',
     'string.min': 'Email should have at least {#limit} characters',
     'string.max': 'Email should have at most {#limit} characters',
@@ -26,17 +26,17 @@ export const createContactSchema = Joi.object({
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(1).max(20).messages({
+  name: Joi.string().min(3).max(20).messages({
     'string.base': 'Name should be a string',
     'string.min': 'Name should have at least {#limit} characters',
     'string.max': 'Name should have at most {#limit} characters',
   }),
-  phoneNumber: Joi.string().min(1).max(20).messages({
+  phoneNumber: Joi.string().min(3).max(20).messages({
     'string.base': 'Phone number should be a string',
     'string.min': 'Phone number should have at least {#limit} characters',
     'string.max': 'Phone number should have at most {#limit} characters',
   }),
-  email: Joi.string().min(1).max(20).email().messages({
+  email: Joi.string().min(3).max(20).email().messages({
     'string.base': 'Email should be a string',
     'string.min': 'Email should have at least {#limit} characters',
     'string.max': 'Email should have at most {#limit} characters',
