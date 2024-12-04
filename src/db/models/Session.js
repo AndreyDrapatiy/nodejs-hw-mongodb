3140125a1257613d96263f75ca6e9afdf9f34fc5
Andrey Drapatiy <drapatiy92@gmail.com>
2024-12-04 12:50:09 +0200

    add register

---
+++ src/db/models/Session.js
@@ -0,0 +1,39 @@
+import { model, Schema } from 'mongoose';
+
+export const sessionSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: Date,
+      required: true
+    },
+    refreshTokenValidUntil: {
+      type: Date,
+      required: true
+    }
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+
+sessionSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+const Session = model('user', sessionSchema);
+export default Session;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,3 +1,17 @@
 import User from '../db/models/User.js';
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
 
-export const registerUser = async (payload) => await User.create(payload);
+export const registerUser = async (payload) => {
+
+  const user = await User.findOne({ email: payload.email });
+
+  if (user !== null) throw createHttpError(409, 'Email already in use');
+
+  const encryptedPassword = await bcrypt.hash(payload.password, 10)
+
+  return await User.create({
+    ...payload,
+    password: encryptedPassword
+  })
+};
